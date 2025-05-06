3850c7f4ea1ccc8b38daa5207bf90212d2f693ec
anderscal <calderonanderson78@gmail.com>
2025-05-05 21:15:59 -0500

    segundo intento var globales

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+      <p>prueba de global</p>
+    </div>
+  )
+}
+
+export default Header
